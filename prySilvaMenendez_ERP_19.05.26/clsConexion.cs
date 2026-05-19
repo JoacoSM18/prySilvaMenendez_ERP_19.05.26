fa451ec44797973142c9c319884fd36848071084
Alumno <Alumno@PC88>
2026-05-19 09:01:18 -0300

    Agregar archivos de proyecto.

---
+++ prySilvaMenendez_ERP_19.05.26/clsConexion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prySilvaMenendez_ERP_19._05._26
+{
+    public class clsConexion
+    {
+
+
+    }
+}
