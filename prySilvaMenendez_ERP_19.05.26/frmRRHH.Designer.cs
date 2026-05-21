c775ab7170de1b88c69bfef64a2ea698e9281e69
JoacoSM18 <joako.sm.18@gmail.com>
2026-05-21 10:03:22 -0300

    Andando

--- prySilvaMenendez_ERP_19.05.26/frmRRHH.Designer.cs
+++ prySilvaMenendez_ERP_19.05.26/frmRRHH.Designer.cs
@@ -30,16 +30,28 @@
         {
             this.grbAgregar = new System.Windows.Forms.GroupBox();
             this.grbEliminar = new System.Windows.Forms.GroupBox();
+            this.lblDNI = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblDomicilio = new System.Windows.Forms.GroupBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.grbAgregar.SuspendLayout();
+            this.lblDomicilio.SuspendLayout();
             this.SuspendLayout();
             // 
             // grbAgregar
             // 
-            this.grbAgregar.Controls.Add(this.label1);
+            this.grbAgregar.Controls.Add(this.lblDomicilio);
+            this.grbAgregar.Controls.Add(this.lblNombre);
+            this.grbAgregar.Controls.Add(this.lblApellido);
+            this.grbAgregar.Controls.Add(this.lblDNI);
+            this.grbAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.grbAgregar.Location = new System.Drawing.Point(12, 16);
             this.grbAgregar.Name = "grbAgregar";
-            this.grbAgregar.Size = new System.Drawing.Size(254, 261);
+            this.grbAgregar.Size = new System.Drawing.Size(254, 353);
             this.grbAgregar.TabIndex = 0;
             this.grbAgregar.TabStop = false;
             this.grbAgregar.Text = "Agregar Usuario";
@@ -48,19 +60,94 @@
             // 
             this.grbEliminar.Location = new System.Drawing.Point(296, 16);
             this.grbEliminar.Name = "grbEliminar";
-            this.grbEliminar.Size = new System.Drawing.Size(300, 261);
+            this.grbEliminar.Size = new System.Drawing.Size(300, 185);
             this.grbEliminar.TabIndex = 1;
             this.grbEliminar.TabStop = false;
             this.grbEliminar.Text = "Eliminar Usuario";
             // 
+            // lblDNI
+            // 
+            this.lblDNI.AutoSize = true;
+            this.lblDNI.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDNI.Location = new System.Drawing.Point(12, 31);
+            this.lblDNI.Name = "lblDNI";
+            this.lblDNI.Size = new System.Drawing.Size(33, 18);
+            this.lblDNI.TabIndex = 0;
+            this.lblDNI.Text = "DNI";
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApellido.Location = new System.Drawing.Point(12, 67);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(59, 18);
+            this.lblApellido.TabIndex = 1;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(12, 107);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(62, 18);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre";
+            this.lblNombre.Click += new System.EventHandler(this.lblNombre_Click);
+            // 
+            // lblDomicilio
+            // 
+            this.lblDomicilio.Controls.Add(this.label4);
+            this.lblDomicilio.Controls.Add(this.label3);
+            this.lblDomicilio.Controls.Add(this.label2);
+            this.lblDomicilio.Controls.Add(this.label1);
+            this.lblDomicilio.Location = new System.Drawing.Point(15, 149);
+            this.lblDomicilio.Name = "lblDomicilio";
+            this.lblDomicilio.Size = new System.Drawing.Size(212, 178);
+            this.lblDomicilio.TabIndex = 3;
+            this.lblDomicilio.TabStop = false;
+            this.lblDomicilio.Text = "Domicilio";
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 47);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(69, 18);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Provincia";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Localidad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Direccion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Geo";
             // 
             // frmRRHH
             // 
@@ -74,6 +161,8 @@
             this.Text = "Recursos Humanos";
             this.grbAgregar.ResumeLayout(false);
             this.grbAgregar.PerformLayout();
+            this.lblDomicilio.ResumeLayout(false);
+            this.lblDomicilio.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -81,7 +170,14 @@
         #endregion
 
         private System.Windows.Forms.GroupBox grbAgregar;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDNI;
         private System.Windows.Forms.GroupBox grbEliminar;
+        private System.Windows.Forms.GroupBox lblDomicilio;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label label4;
     }
 }
--- prySilvaMenendez_ERP_19.05.26/frmRRHH.cs
+++ prySilvaMenendez_ERP_19.05.26/frmRRHH.cs
@@ -16,5 +16,10 @@ namespace prySilvaMenendez_ERP_19._05._26
         {
             InitializeComponent();
         }
+
+        private void lblNombre_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
