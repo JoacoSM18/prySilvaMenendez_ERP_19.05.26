b94da0504dc83c3048974e0e5f6e6696a8ffbb5d
JoacoSM18 <joako.sm.18@gmail.com>
2026-07-01 14:07:38 -0300

    Guardo, falta la consulta

--- prySilvaMenendez_ERP_19.05.26/frmAgregarDatosContacto.cs
+++ prySilvaMenendez_ERP_19.05.26/frmAgregarDatosContacto.cs
@@ -42,9 +42,9 @@ namespace prySilvaMenendez_ERP_19._05._26
             clsConexion.ConexionBaseDeDatos.Desconectar();
             clsConexion.ConexionBaseDeDatos.Conectar();
             DataTable tabla = clsConexion.ConexionBaseDeDatos.Consultar(
-    "SELECT * FROM Usuario WHERE Gmail = ''");
+    "SELECT * FROM Usuario WHERE RedSocialPrincipal = ''");
 
-            MessageBox.Show(tabla.Rows.Count.ToString());
+            MessageBox.Show(tabla.Rows.Count.ToString());   
             foreach (DataRow fila in tabla.Rows)
             {
                 string usuario = fila["Nombre"].ToString() + " " + fila["Apellido"].ToString();
