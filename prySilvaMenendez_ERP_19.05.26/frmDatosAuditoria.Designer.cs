69a2dc3c41a84dcd59b11758bd125c6aff8a63e4
JoacoSM18 <joako.sm.18@gmail.com>
2026-07-02 00:56:43 -0300

    listo para mostrar

--- prySilvaMenendez_ERP_19.05.26/frmDatosAuditoria.Designer.cs
+++ prySilvaMenendez_ERP_19.05.26/frmDatosAuditoria.Designer.cs
@@ -163,9 +163,9 @@
             // btnFiltrar
             // 
             this.btnFiltrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnFiltrar.Location = new System.Drawing.Point(806, 81);
+            this.btnFiltrar.Location = new System.Drawing.Point(806, 74);
             this.btnFiltrar.Name = "btnFiltrar";
-            this.btnFiltrar.Size = new System.Drawing.Size(160, 33);
+            this.btnFiltrar.Size = new System.Drawing.Size(160, 40);
             this.btnFiltrar.TabIndex = 22;
             this.btnFiltrar.Text = "Filtrar";
             this.btnFiltrar.UseVisualStyleBackColor = true;
